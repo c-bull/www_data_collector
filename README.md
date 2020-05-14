199680bbba1c38fa2a112d543ad01d543c39548d
git <git@example.com>
2020-05-14 13:00:00 +0200

    options parsing and verbose output

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # www_data_collector
-This is a piece of python code that aims to facilitate extraxtion of arbitrary data from websites.
-It can be used to extract specific data from websites that present search results to users in a form of hyperlinks list (e.g. ecommerce sites, catalogs, web archives)
+This is a piece of python code that aims to facilitate extraction of arbitrary data from websites.
+It can be used to extract specific data from websites that present search results to users in a form of hyperlinks list (e.g. ecommerce sites, catalogs, web archives).
 The tool crawls through the result pages and uses BeautifulSoup's HTML parser to collect pieces of information specified in the JSON config file.
 Collected data is stored in a JSON format and saved to file. 
 Typically, the data requires cleaning and removing artefacts that are present in HTML before it can be stored in a database. 
-This means that another use-case specifi script needs to be prepared to process collected data.
+This means that another use-case specific script needs to be prepared to process collected data.
 Alternatively, www_data_collector can be extended by adding code to the three handler functions (handlers.py):
-* result_url_handler - Run before data collection from each resultpage. It is intended to verify or modify result page URL if needed.
-* result_page_processing_handler - Run after each data collection. It is intended extend data collection for more complex cases (potentially adding subsequent requests based on the results from each page).
-* stop_condition_handler - Run after each data collection. By default returnes False so the crawler loop does not stop (it stops eventually when there are no more result links available). It is intended to define arbitrary stop condition. Useful when the results are sorted chronologically and we know we only have to crawl new results.
+* result_url_handler - Run each time before data is collected from a result page. It is intended to verify or modify result page URL if needed.
+* result_page_processing_handler - Run after each data collection. It is intended extend data collection for more complex cases (potentially adding subsequent requests based on the collected data).
+* stop_condition_handler - Run after each data collection. By default it returnes False, so the crawler loop does not stop (it stops eventually when there are no more result links available). It is intended to define arbitrary stop condition. Useful when the results are sorted chronologically and we know we only have to crawl new results.
--- main.py
+++ main.py
@@ -5,22 +5,10 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from tld import get_tld
 import time
+import uuid
 from common import Common
 import handlers
 
-
-base_url = ""
-page_parameter_name = ""
-first_page = 1
-last_page = 0
-interval = 0
-stop_condition = False
-
-result_url_tag = ""
-result_url_class = ""
-visit_results_with_different_domain = False
-
-items_to_extract = ""
 
 #number of result pages processed
 counter = 0
@@ -28,27 +16,70 @@ counter = 0
 #datetime of the crawler initiation
 crawl_start_time = datetime.today()
 
+output = []
+
 #loads config file
 def load_config(config_file_path):
     with open(config_file_path) as config_file:
         config_file_json = json.load(config_file)
 
-    global base_url, page_parameter_name, first_page, last_page, interval, stop_condition, result_url_tag, result_url_class, items_to_extract, visit_results_with_different_domain
-    base_url = config_file_json['base_url']
-    page_parameter_name = config_file_json['page_parameter_name']
-    first_page = config_file_json['first_page']
-    #if last_page is 0 and there is no stop condition the crawler will run till there is no results on page
-    last_page = config_file_json['last_page']
-    interval = config_file_json['interval']
-    #if stop condition is set to true the stop_condition_handler function will run at the end of crawl of each page
-    stop_condition = config_file_json['stop_condition']
-    result_url_tag = config_file_json['result_url_tag']
-    result_url_class = config_file_json['result_url_class']
-    items_to_extract = config_file_json['items_to_extract']
-    visit_results_with_different_domain = config_file_json['visit_results_with_different_domain']
+    global base_url, page_parameter_name, first_page, last_page, interval, result_url_tag, result_url_class, items_to_extract, visit_results_with_different_domain, output_file, verbose
+    if "base_url" in config_file_json:
+        base_url = config_file_json["base_url"]
+    else:
+        print("Base URL not present in the config file. Quitting.")
+        quit()
+    if "page_parameter_name" in config_file_json:
+        page_parameter_name = config_file_json["page_parameter_name"]
+    else:
+        print("Page parameter name not present in the config file. Quitting.")
+        quit()  
+    if "first_page" in config_file_json:
+        first_page = config_file_json["first_page"]
+    else:
+        first_page = 1
+    #if last_page is 0 and there is no stop condition the crawler will run until there are no results on current page
+    if "last_page" in config_file_json:
+        last_page = config_file_json["last_page"]
+    else:
+        last_page = 0
+    if "interval" in config_file_json:
+        interval = config_file_json["interval"]
+    else:
+        print("Warning: default interval of 0 seconds is used.")
+        interval = 0
+    if "result_url_tag" in config_file_json:
+        result_url_tag = config_file_json["result_url_tag"]
+    else:
+        result_url_tag = "a"
+    if "result_url_class" in config_file_json:
+        result_url_class = config_file_json["result_url_class"]
+    else:
+        print("Result URL class not specified in the config file. Quitting.")
+    #there might be no items to extract specified
+    #still the crawler will run and handlers might be used to extract relevant data
+    if "items_to_extract" in config_file_json:
+        items_to_extract = config_file_json["items_to_extract"]
+    else:
+        items_to_extract = []
+    #by default we only visit result pages on the crawled domain
+    if "visit_results_with_different_domain" in config_file_json:
+        visit_results_with_different_domain = config_file_json["visit_results_with_different_domain"]
+    else:
+        visit_results_with_different_domain = False
+    if "verbose" in config_file_json:
+        verbose = config_file_json["verbose"]
+    else:
+        verbose = False
+    if "output_file" in config_file_json:
+        output_file = config_file_json["output_file"]
+    else:
+        print("No output file specified. Everything will be preinted to the screen.")
+        output_file = ""
+        verbose = True
 
 def crawl():
-    global last_page, counter
+    global last_page, counter, output
     stop_condition_reached = False
     current_page = first_page
     #if last page was 0 (not defined) we set it to large number so the loop condition makes sense
@@ -64,6 +95,9 @@ def crawl():
         soup = BeautifulSoup(response_text, features="html.parser")
         #extract result urls
         result_urls = soup.find_all(result_url_tag, {"class" : result_url_class})
+        #stop if there are no more results
+        if result_urls == []:
+            break
         for result_url in result_urls:
             #result url handler checks if the result url is valid
             #it can modify the url or set it to empty string to skip the result
@@ -83,15 +117,16 @@ def crawl():
                 #by default is does not do anything
                 handlers.result_page_processing_handler(result_soup)
                 counter += 1
-                #if the stop condition is used run stop_condition_handler
-                if stop_condition == True:
-                    stop_condition_reached = handlers.stop_condition_handler(result_soup)
+                stop_condition_reached = handlers.stop_condition_handler(result_soup)
             #if the stop condition is reached then end crawling
             if stop_condition_reached == True:
                 break
+        if verbose:
+            print("Page " + str(current_page) + " crawled.")
         current_page += 1
 
 def extract_data(result_soup):
+    global output
     for item in items_to_extract:
         #three types of filters are supported: tag, regex, sieve
         #first: tag name with specific attributes.
@@ -100,34 +135,40 @@ def extract_data(result_soup):
             #this requires specifing attr_to_extract in the config file
             if "attr_to_extract" in item:
                 try:
-                    print(result_soup.find(item["html_tag_name"], {item["html_tag_attr_name"] : item["html_tag_attr_value"]})[item["attr_to_extract"]])
+                    if verbose:
+                        print(result_soup.find(item["html_tag_name"], {item["html_tag_attr_name"] : item["html_tag_attr_value"]})[item["attr_to_extract"]])
                     continue
                 except:
                     pass
             #by default we are trying to collect .text inside a tag
             try:
-                print(result_soup.find(item["html_tag_name"], {item["html_tag_attr_name"] : item["html_tag_attr_value"]}).text)
+                if verbose:
+                    print(result_soup.find(item["html_tag_name"], {item["html_tag_attr_name"] : item["html_tag_attr_value"]}).text)
                 continue
             except:
                 pass
-            print("Item '" + item["name"] + "' not found")
+            if verbose:
+                print("Item '" + item["name"] + "' not found")
         #second: tag name with regex. regex is applied to string inside a tag.
         if item["filter_type"] == "regex":
             #check if the value to be collected is a tag attribute
             #this requires specifing attr_to_extract in the config file
             if "attr_to_extract" in item:
                 try:
-                    print(result_soup.find(item["html_tag_name"], string=re.compile(item["regex_string"]))[item["attr_to_extract"]])
+                    if verbose:
+                        print(result_soup.find(item["html_tag_name"], string=re.compile(item["regex_string"]))[item["attr_to_extract"]])
                     continue
                 except:
                     pass
             #by default we are trying to collect .text inside a tag
             try:
-                print(result_soup.find(item["html_tag_name"], string=re.compile(item["regex_string"])).text)
+                if verbose:
+                    print(result_soup.find(item["html_tag_name"], string=re.compile(item["regex_string"])).text)
                 continue
             except:
                 pass
-            print("Item '" + item["name"] + "' not found")
+            if verbose:
+                print("Item '" + item["name"] + "' not found")
         #third: sieve selector consisting series of tags used to traverse the document.
         if item["filter_type"] == "sieve":
             #check if the value to be collected is a tag attribute
@@ -135,23 +176,27 @@ def extract_data(result_soup):
             if "attr_to_extract" in item:
                 try:
                     for found_item in result_soup.select(item["sieve_selector"]):
-                        print(found_item[item["attr_to_extract"]])
+                        if verbose:
+                            print(found_item[item["attr_to_extract"]])
                     continue
                 except:
                     pass
             #by default we are trying to collect .text inside a tag
             try:
                 for found_item in result_soup.select(item["sieve_selector"]):
-                    print(found_item.text)
+                    if verbose:
+                        print(found_item.text)
                 continue
             except:
                 pass
-            print("Item '" + item["name"] + "' not found")
+            if verbose:
+                print("Item '" + item["name"] + "' not found")
 
 def main(config_file_path):
     load_config(config_file_path)
     crawl()
-    print(counter)
+    if verbose:
+        print("Finished. Crawled " + str(counter) + " result pages.")
 
 
 if __name__ == '__main__':
